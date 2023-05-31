409313140ec22eff54ebe1864e1267a6ce444560
Anton Nahhas <antonna@edu.hac.ac.il>
2023-05-31 19:21:35 +0300

    implemented a third feature, improved design of code

--- pptxApp.py
+++ pptxApp.py
@@ -1,4 +1,3 @@
-# -----------------------------MODULE SECTION-------------------------------------------------------------------
 import time
 from pptx import Presentation
 import openai
@@ -7,58 +6,56 @@ import os
 import re
 import asyncio
 
-# -----------------------------CONSTANT SECTION-------------------------------------------------------------------
 openai.api_key = os.environ.get('API_KEY')
 CONTENT = [
     {"role": "system", "content": "Can you explain the slides in basic english, and provide examples if needed!"}
 ]
 ERROR_MESSAGE = "Something is wrong:"
 ENGINE_MODEL = "gpt-3.5-turbo"
+WRITE_TO_FILE_MODE = 'w'
 
 
-# -----------------------------METHOD SECTION-------------------------------------------------------------------
 async def parse_presentation(presentation_path):
     """
-    this method receives a path for a pptx presentation, checks if the path is found in the operating system, if so,
-    parses the data to slides. the method, returns a list of explanations.
-    :param: presentation_path: path of a power-point presentation (String)
-    :return: list of explanations (List of strings)
+    This method receives a path for a pptx presentation, checks if the path is found in the operating system, if so,
+    parses the data to slides. The method, returns a list of explanations.
+    :param: presentation_path: path of a power-point presentation. (String)
+    :return: list of explanations. (List of strings)
     """
-    if not os.path.isfile(presentation_path):  # check if path is available
+    # check if path is available
+    if not os.path.isfile(presentation_path):
         print(f"{ERROR_MESSAGE} the path you provided does not exist.")
         return []
 
     prs = Presentation(presentation_path)
     explanations = []
     for slide in prs.slides:
-        explanation = await parse_slide_of_pptx(slide)  # call method to parse data in each slide
+        explanation = await parse_slide_of_pptx(slide)
         explanations.append(explanation)
     return explanations
 
 
-# --------------------------------------------------------------------------------------------------------------------
 async def parse_slide_of_pptx(slide):
     """
-    this method receives a single slide, the method parses the data on that slide, it calls another method to get the
-    response and return it to the parse_presentation method. it throws an error if an exception occurred.
-    :param: slide: a single slide from the power-point (slide object)
-    :return: the explanation if the processing went well, an error, otherwise. (List of Strings)
+    This method receives a single slide, the method parses the data on that slide, it calls another method to get the
+    response and return it to the parse_presentation method. It throws an error if an exception occurred.
+    :param: slide: A single slide from the power-point. (slide object)
+    :return: The explanation if the processing went well, an error, otherwise. (List of Strings)
     """
     try:
-        response = await request_completion(slide)  # call a function to get explanations on each slide.
+        response = await request_completion(slide)
         return response
-    except Exception as error:  # if an error occurred, display a suitable message to user.
+    except Exception as error:
         error_message = f"{ERROR_MESSAGE} Error processing slide: {str(error)}"
         return error_message
 
 
-# --------------------------------------------------------------------------------------------------------------------
 def parse_text_of_slide(slide):
     """
-    this method receives a slide, and extracts all the extractable text found in that slide. In addition, it cleans
+    This method receives a slide, and extracts all the extractable text found in that slide. In addition, it cleans
     the data using the strip function.
-    :param: slide: a single slide from the power-point (Slide Object)
-    :return: explanation response (String)
+    :param: slide: A single slide from the power-point. (Slide Object)
+    :return: Explanation response. (String)
     """
     slide_text = []
     for shape in slide.shapes:
@@ -70,48 +67,48 @@ def parse_text_of_slide(slide):
     return " ".join(slide_text)
 
 
-# --------------------------------------------------------------------------------------------------------------------
 async def request_completion(slide):
     """
-    this method receives a slide object, sends a request to the openai API asking the server to explain the content
-    of that slide, the method returns the response from the API
-    :param: slide: slide: a single slide from the power-point (Slide Object)
-    :return: response of the API
+    This method receives a slide object, sends a request to the openai API asking the server to explain the content
+    of that slide, the method returns the response from the API.
+    :param: slide: Slide: a single slide from the power-point. (Slide Object)
+    :return: Response of the API.
     """
     CONTENT.append({"role": "user", "content": parse_text_of_slide(slide)})
     response = await openai.ChatCompletion.acreate(
-        model=ENGINE_MODEL,  # using the chat gpt-3.5-turbo model to get strong responses
+        model=ENGINE_MODEL,
         messages=CONTENT
     )
     content = response["choices"][0].message.content
-    cleaned_content = clean_text(content)  # clean content from unwanted characters.
+    cleaned_content = clean_text(content)
     return cleaned_content
 
 
-# --------------------------------------------------------------------------------------------------------------------
 def clean_text(text):
     """
-    this method receives the response retrieved from the openai API and cleans it, in other words, gets rid of
+    This method receives the response retrieved from the openai API and cleans it, in other words, gets rid of
     unwanted characters.
-    :param: text: response from API (string)
-    :return: clean version of the response (string)
+    :param: text: Response from API. (string)
+    :return: clean Version of the response. (string)
     """
     cleaned_text = re.sub(r"\n", "", text)
     cleaned_text = cleaned_text.encode("ascii", "ignore").decode("utf-8")
     return cleaned_text.strip()
 
 
-# --------------------------------------------------------------------------------------------------------------------
 def save_explanations(explanations, presentation_path):
     """
-    this method receives a list of strings each string implements an explanation, creates a JSON file and appends
+    This method receives a list of strings each string implements an explanation, creates a JSON file and appends
     the explanations to the file, in the format of (slide#: explanations).
-    :param: explanations: list of explanation retrieved from the API (List of strings)
+    :param: explanations: List of explanation retrieved from the API. (List of strings)
     :return:
     """
-    presentation_name = os.path.basename(presentation_path)  # get the base name of power-point from path
-    presentation_name = os.path.splitext(presentation_name)[0]  # remove file's extension
-    output_file = f"{presentation_name}_explanations.json"  # name the output file, with the name of power-point
+    # get the base name of power-point from path
+    presentation_name = os.path.basename(presentation_path)
+    # remove file's extension
+    presentation_name = os.path.splitext(presentation_name)[0]
+    # name the output file, with the name of power-point
+    output_file = f"{presentation_name}_explanations.json"
     slide_explanations = {}
 
     for slide_num, explanation in enumerate(explanations, start=1):
@@ -119,20 +116,19 @@ def save_explanations(explanations, presentation_path):
         slide_explanations[slide_key] = explanation
 
     try:
-        if not os.path.exists(output_file):  # create file if it does not exist.
-            open(output_file, 'w').close()
+        if not os.path.exists(output_file):
+            open(output_file, WRITE_TO_FILE_MODE).close()
 
-        with open(output_file, 'w') as file:
+        with open(output_file, WRITE_TO_FILE_MODE) as file:
             json.dump(slide_explanations, file, indent=4)
         print(f"Explanations saved to {output_file}")
     except IOError as error:
         print(f"{ERROR_MESSAGE} Error saving explanations: {str(error)}")
 
 
-# --------------------------------------------------------------------------------------------------------------------
 async def main(presentation_path):
     """
-    this is the main method, uses asyncio handling to await the requests sent to the openai API, and handles saving
+    This is the main method, uses asyncio handling to await the requests sent to the openai API, and handles saving
     the responses to the output file.
     :param: presentation_path: string of a power-point path. (string)
     :return:
@@ -141,18 +137,26 @@ async def main(presentation_path):
     save_explanations(explanations, presentation_path)
 
 
-# --------------------------------------------------------------------------------------------------------------------
+def get_input():
+    """
+    The method asks the user for a valid path.
+    :return: Return the input provided by user.
+    """
+    input_message = "Please provide the path of the PowerPoint presentation: "
+    return input(input_message)
+
+
 if __name__ == "__main__":
     """
-    the controller of the app, handles the inputs from the user, prints relevant messages and runs the main program
+    The controller of the app, handles the inputs from the user, prints relevant messages and runs the main program.
     """
-    presentation_path = input("Please provide the path of the PowerPoint presentation: ")
-    print("Processing PowerPoint...")  # inform the user to wait until the program finishes running
-    start_time = time.time()  # compute time of execution
+    presentation_path = get_input()
+    print("Processing PowerPoint...")
+    start_time = time.time()
 
-    asyncio.run(main(presentation_path))  # run main program
+    asyncio.run(main(presentation_path))
 
     end_time = time.time()
     execution_time = end_time - start_time
     minutes, seconds = divmod(execution_time, 60)
-    print(f"Execution time: {minutes:.0f} minutes {seconds:.2f} seconds")  # print total time of execution
+    print(f"Execution time: {minutes:.0f} minutes {seconds:.2f} seconds")
