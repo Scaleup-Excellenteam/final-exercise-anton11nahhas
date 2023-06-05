afbd909e20cd9a95510ec425071e880cbd82009a
Anton Nahhas <antonna@edu.hac.ac.il>
2023-06-05 21:42:41 +0300

    fixed the system test, now it has a single functional test, that checks both of the methods upload and status in the PythonClient class

--- Explainer_test.py
+++ Explainer_test.py
@@ -1,43 +1,50 @@
 import unittest
 import subprocess
-import requests
-import openai
-from flask import Flask
-import re
+
+from PythonClient import PythonClient
 
 
 class SystemTest(unittest.TestCase):
-    def setUp(self):
-        self.web_api_process = subprocess.Popen(["python", "webAPI.py"])
-        self.explainer_process = subprocess.Popen(["python", "pptxAPP.py"])
+    @classmethod
+    def setUpClass(cls):
+        """
+        This method starts a subprocess to run both the webAPI and the pptxApp files.
+        :return:
+        """
+        cls.web_api_process = subprocess.Popen(["python", "webAPI.py"])
+        cls.explainer_process = subprocess.Popen(["python", "pptxApp.py"])
 
-    def tearDown(self):
-        self.web_api_process.terminate()
-        self.explainer_process.terminate()
+    @classmethod
+    def tearDownClass(cls):
+        """
+        This method terminates the subprocesses started before
+        :return:
+        """
+        cls.web_api_process.terminate()
+        cls.explainer_process.terminate()
 
     def test_upload_and_check_status(self):
-        upload_process = subprocess.Popen(["python", "PythonClient.py"], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-                                          text=True)
-        upload_process.stdin.write("u\n")
-        upload_process.stdin.flush()
-        upload_process.stdin.write(r"C:\Users\User\Desktop\New folder\test.pptx")
-        upload_process.stdin.flush()
-        output, _ = upload_process.communicate()
+        """
+        This method starts the System test where it uses the PythonClient class to create an object that listens
+        to the server (port::5000) adds a power-point path (the presentation is found on my computer please provide
+        another one if needed, thanks) and uses both of the methods in that class to upload the file, retrieve the
+        UID and check its status. Since the tests are run immediately one after the other, then the status retrieved
+        must be 'pending', meaning the file has not been processed yet. Basically, the method test, if the status is
+        pending then the explanation must be None, else there must be an explanation and not none.
+        :return:
+        """
+        client = PythonClient("http://localhost:5000")
 
-        uid_match = re.search(r"Uploaded file with UID:\s*([a-fA-F\d-]+),", output)
-        uid = uid_match.group(1) if uid_match else None
-
+        powerpoint_path = r"C:\Users\User\Desktop\New folder\test.pptx"
+        uid = client.upload(powerpoint_path)
         self.assertIsNotNone(uid, "UID not found in the output")
 
-        status_process = subprocess.Popen(["python", "PythonClient.py"], stdin=subprocess.PIPE, stdout=subprocess.PIPE,
-                                          text=True)
-        status_process.stdin.write("s\n")
-        status_process.stdin.flush()
-        status_process.stdin.write(uid + "\n")
-        status_process.stdin.flush()
-        status_output, _ = status_process.communicate()
+        status = client.status(uid)
 
-        self.assertTrue("200 OK" in status_output, "Status code not found in the output")
+        if status.status == 'pending':
+            self.assertEqual(status.explanation, 'None')
+        elif status.status == 'done':
+            self.assertIsNone(status.explanation, "Explanation should be None")
 
 
 if __name__ == '__main__':
